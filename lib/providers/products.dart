72ae89f73ed97c95c1b6436d5fe97f317a0051a2
Neil Ye <neil.ye@cloud-interactive.com>
2021-01-04 08:37:09 +0800

    241. Showing a Loading Indicator

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -67,10 +67,10 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  void addProduct(Product product) {
+  Future<void> addProduct(Product product) {
     const url =
         'https://flutter-update-ddb4b-default-rtdb.firebaseio.com/products.json';
-    http
+    return http
         .post(
       url,
       body: json.encode({
